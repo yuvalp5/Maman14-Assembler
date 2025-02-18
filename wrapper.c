92e7e1a42715a72d8409f316bce7cee613984a54
Oren <oren.dev@icloud.com>
2025-02-19 00:49:50 +0200

    Work on prototype for wrapper

--- wrapper.c
+++ wrapper.c
@@ -3,6 +3,32 @@
  * @brief Wrapper component to be accessed by user. Recive input here, so on...
  */
 
-int main() {
-    printf("%s", "Hello World!");
+int main(int argc, char* argv[]) {
+    // argc- how many args were passed
+    // argv - args themselves, @0 is the name of the program (either wrapper.c or final binary name)
+
+    /*
+    What to do in wrapper:
+    1. pre_assembler
+    1.5 check failiures (mayvbe not here)
+    2. first and second pass
+    3. output file
+
+    report throughout for debug
+    */
+    if (argc <= 1) {
+        printf("No arguments passed\n");
+    }
+    else {
+        printf("[DEBUG:]Arguments passed: \n");
+        printf("[DEBUG:]%d\n", argc);
+        for (int i = 1; i < argc; i++) {
+            printf("%s\n", argv[i]);
+        }
+
+        printf("[DEBUG:]now passing to inter...\n");
+
+    }
+
+    return 0;
 }
---
+++ wrapper.h
@@ -0,0 +1,7 @@
+/**
+ * @brief Wrapper component to be accessed by user. Used to recive input and pass it on through all stages as well as to print the output to the user.
+ */
+
+#include <stdio.h>
+#include "inter/pre_assembler.h"
+//#include "core/headers.h"
