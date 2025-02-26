8ac910d1ba86ac34b1fb0d68fcd0ca25ff051e02
unknown <yuvalpeled5@gmail.com>
2025-02-26 18:36:07 +0200

    Add #defines

--- inter/pre_assembler.h
+++ inter/pre_assembler.h
@@ -3,8 +3,11 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "../shared/types.h"
-
+#define MAX_LINE_LEN 256
+#define MACRO_START_KW "mcro"
+#define MACRO_END_KW "mcroend"
  /**
   * @brief Pre-assembler method to perform algorythm, rsulting file is saved (where?)
   */
@@ -12,7 +15,7 @@ void pre_assembler(int text, int save_loc);
 
 // these should maybe be internal (only in .c)
 void pre_assembler(int text, int save_loc);
-char** ln_to_fields(int ln_num);
+char* read_next_line(char *file_name, int ln_num);
 void replace_macro(int text);
 void add_macro(int text);
 int save_file(int text, char* save_loc[]);
