89f987f9a303b28dba276a91371efe361f58d44f
Oren <oren.dev@icloud.com>
2025-03-05 01:46:02 +0200

    add TODOs

--- shared/utils.c
+++ shared/utils.c
@@ -1,7 +1,10 @@
+//TODO:idea for file handling workflow- implement replace_line function which will get a file and position and text, and will replace that line with given text
+//TODO: think over methods to work on a file
+
 #include "utils.h"
-  //split the line to fields with strtok
+//split the line to fields with strtok
     //return the pointer to the first field
-char* ln_to_fields(FILE *file, int ln_num, char *buffer, int size) {
+char* ln_to_fields(FILE* file, int ln_num, char* buffer, int size) {
     if (file == NULL || ln_num < 1 || buffer == NULL) {
         return NULL;  // Handle invalid input
     }
@@ -11,9 +14,9 @@ char* ln_to_fields(FILE *file, int ln_num, char *buffer, int size) {
 
     while (fgets(buffer, size, file) != NULL) {  // read file to line
         if (current_line == ln_num) {
-            char *field = strtok(buffer, " \t");  // Extract the first field
+            char* field = strtok(buffer, " \t");  // Extract the first field
             return field;  // Return the pointer to the first field
         }
         current_line++;
     }
-
+}
