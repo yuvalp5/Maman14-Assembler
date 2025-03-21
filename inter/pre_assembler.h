f0a94ded8083e558f7df2e5d9695d3db97d3815e
unknown <yuvalpeled5@gmail.com>
2025-03-21 23:12:35 +0200

    Implement fully working pre assembler + relevant header

--- inter/pre_assembler.h
+++ inter/pre_assembler.h
@@ -1,33 +1,145 @@
 /**
- * @brief Pre-assembler implementation as per P. 40
+ * @file pre_assembler.h
+ * @brief Header file for the pre-assembler module
+ *
+ * This module handles the first phase of assembly: macro expansion.
+ * It takes an assembly source file (.as) and produces an expanded
+ * file (.am) where all macro invocations have been replaced with
+ * their content.
+ *
+ *
  */
-#ifndef TYPES_H
-#define TYPES_H
-#include "../shared/types.h"
-#endif
 
-#ifndef UTILS_H
-#define UTILS_H
-#include "../shared/utils.h"
-#endif
-#ifndef STDIO_H
-#define STDIO_H
+#ifndef PRE_ASSEMBLER_H
+#define PRE_ASSEMBLER_H
+
 #include <stdio.h>
-#endif
-#ifndef DEFINITIONS_H
-#define DEFINITIONS_H
-#include "../wrapper.h"
-#endif
 
 /**
- * @brief Pre-assembler method to perform algorythm
- * @param open_loc File to open
- * @param save_loc File to save to
+ * @brief Maximum length of a line in the assembly file
  */
-void pre_assembler(char *src, char *dest); // todo int with success/fail output
-int macro_definition(char *field);
-void replace_macro(FILE *file, const char *line);
-int add_macro(FILE *file);
-char *get_macro(const char *macro_name);
+#define MAX_LINE_LEN 80
 
-int is_macro;
+/**
+ * @brief Maximum number of macros that can be defined
+ */
+#define MAX_MACROS 100
+
+/**
+ * @brief Maximum length of a macro name
+ */
+#define MAX_MACRO_NAME_LEN 32
+
+/**
+ * @brief Maximum length of the content of a macro
+ */
+#define MAX_MACRO_CONTENT_LEN 1000
+
+/**
+ * @brief Structure to represent a macro
+ * Contains the macro's name and its content (all the lines between
+ * "mcro" and "mcroend" directives).
+ */
+typedef struct {
+    /** The name of the macro */
+    char name[MAX_MACRO_NAME_LEN];
+
+    /** The content of the macro */
+    char content[MAX_MACRO_CONTENT_LEN];
+} Macro;
+
+/*
+ * @brief Global array of macros
+ */
+extern Macro macro_table[MAX_MACROS];
+
+/**
+ * @brief Global counter for the number of defined macros
+ */
+extern int macro_count;
+
+/**
+ * @brief Main function for pre-assembler - processes macros in source file and creates an expanded file
+ *
+ * This function performs the macro expansion process:
+ * 1. Opens the source file and destination file
+ * 2. Processes macro definitions and invocations
+ * 3. Writes the expanded code to the destination file
+ *
+ * @param src Path to the source assembly file (.as)
+ * @param dest Path for the expanded output file (.am)
+ * @return 0 on success, non-zero on failure
+ */
+int pre_assembler(char *src, char *dest);
+
+/**
+ * @brief Extracts macro name from a macro definition line
+ *
+ * Given a line containing "mcro [name]", this function extracts the
+ * name part. It handles whitespace between the "mcro" keyword and
+ * the name.
+ *
+ * @param line The macro definition line (starting with "mcro")
+ * @param macro_name Buffer to store the extracted name
+ * @return 1 on success, 0 on failure
+ */
+int extract_macro_name(const char *line, char *macro_name);
+
+/**
+ * @brief Extract the first word from a line
+ *
+ * This is used to identify lines that might be macro invocations
+ * or special directives like "mcro" or "mcroend".
+ *
+ * @param line The input line
+ * @param word Buffer to store the extracted first word
+ */
+void extract_first_word(const char *line, char *word);
+
+/**
+ * @brief Add a new macro to the macro table
+ *
+ * Performs validation checks and adds a new macro to the global table.
+ * The macro is initially created with empty content, which will be
+ * populated by subsequent calls to append_to_macro().
+ *
+ * @param name Name of the macro to add
+ * @return 1 on success, 0 on failure
+ */
+int add_macro(const char *name);
+
+/**
+ * @brief Append a line to an existing macro's content
+ *
+ * This function is called for each line between "mcro" and "mcroend"
+ * to build up the macro's content buffer.
+ *
+ * @param name Name of the macro to append to
+ * @param line Line to append to the macro's content
+ * @return 1 on success, 0 on failure
+ */
+int append_to_macro(const char *name, const char *line);
+
+/**
+ * @brief Get the content of a macro by name
+ *
+ * Used during macro expansion to get the content that should replace
+ * a macro invocation.
+ *
+ * @param name Name of the macro to look up
+ * @return Pointer to the macro's content, or NULL if macro not found
+ */
+char* get_macro_content(const char *name);
+
+/**
+ * @brief Check if a macro name is valid (not a reserved word)
+ *
+ * Ensures that macro names don't conflict with assembler instructions,
+ * directives, or registers.
+ *
+ * @param name The macro name to check
+ * @return 1 if valid, 0 if invalid
+ */
+int is_valid_macro_name(const char *name);
+
+#endif /* PRE_ASSEMBLER_H */
