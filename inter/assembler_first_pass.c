9c9d7960d3f044b7412880010dc9af4d8710f783
unknown <yuvalpeled5@gmail.com>
2025-03-22 17:34:11 +0200

    Start implementation of the main algorithm - very initial

--- inter/assembler_first_pass.c
+++ inter/assembler_first_pass.c
@@ -1,10 +1,40 @@
 #include "assembler_first_pass.h"
 
-void first_pass() {
+void first_pass(*char src) {
     // 1
     int IC = 100;
     int DC = 0;
     int is_symbol = 0;
+    char line[MAX_LINE_LEN];
+    char first_word[MAX_LINE_LEN];
+    FILE *input_pre_assembled;
+
+    input_pre_assembled = fopen(src, "r");
+    while (fgets(line, MAX_LINE_LEN, input_pre_assembled) !=NULL) {
+       first_word = split_line_to_fields(line); // create in utils
+       if (is_symbol(first_word)) {
+           is_symbol = 1;
+       }
+       if (is_storage(first_word)) {
+           add_symbol_to_table(symbol_name, DC);
+           //raise error if the symbol is already in the table
+           // stage 7
+       }
+       if(!(is_extern || is_entry)){
+           //stage 11
+           }
+       if (is_extern) {
+           //to stage 11
+           add_symbol_to_table
+       }
+       if (is_entry) {
+           // to stage 2
+       }
+
+
+
+
+
     // 4 new symbol definition
     // 16  -update IC+L -> IC and go to to stage 2
     // 17 keep track of all errors - dont stop until end
