06fcf9dbb8230256e172dc3973afff2220c08d89
Oren <oren.dev@icloud.com>
2025-03-29 21:49:02 +0300

    delete unittester src

--- io/testing/tester.c
+++
@@ -1,9 +0,0 @@
-#include "../../inter/pre_assembler.h"
-#include <stdio.h>
-
-int main() {
-    char fname[] = "io/example1_success.as";
-    char fname2[] = "io/example1_success.am";
-    pre_assembler(fname, fname2);
-    return 0;
-}
